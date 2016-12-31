b4686c62ddd4a7a0b3e067c11ee481d79170101d
Lee Muro <Lee Muro>
2016-12-31 00:33:21 -0500

    adds more categorization

--- categories.elm
+++ categories.elm
@@ -20,7 +20,10 @@ defaultCategories = [
         , "pomelo", "quince", "raspberry", "salmonberry", "rambutan", "redcurrant", "salak", "satsuma"
         , "strawberry", "tamarillo", "tamarind", "yuzu"
         ]
-    , exclusions = [ "can", "powder", "tsp", "tbsp", "frozen", "juice", "jam", "jelly", "sauce", "with green chilis" ]
+    , exclusions = 
+        [ "can", "dried", "dry", "powder", "tsp", "tbsp", "teaspoon", "tablespoon", "frozen"
+        , "juice", "jam", "jelly", "sauce", "with green chilis" 
+        ]
     }
   , { name = "Meat"
     , matchers = 
@@ -29,17 +32,25 @@ defaultCategories = [
         ] 
     , exclusions = [ "can", "broth", "stock", "bouillon" ]
     }  
+  , { name = "Breads/Baked Goods"
+    , matchers = [ "bread", "tortilla", "wrap", "pita", "muffin", "bun", "roll" ]
+    , exclusions = [ "chips", "cinnamon rolls" ]
+    }
   , { name = "Canned/Jarred Goods"
     , matchers = [ "can", "jar", "diced tomato", "crushed tomato", "tomato sauce", "broth", "soup", "stock", "olive" ]
     , exclusions = []
     }  
+  , { name = "Condiments"
+    , matchers = [ "ketchup", "mustard", "relish", "sauce", "mayo" ]
+    , exclusions = [ "tomato sauce", "pasta sauce" ]
+    }
   , { name = "Spices/Herbs"
     , matchers = 
         [ "allspice", "all spice", "spice", "anise", "basil", "bay leaf", "caraway", "cardamom", "cayenne", "pepper"
         , "celery seed", "chicory", "chili powder", "dill", "garlic", "chives", "cilantro", "cinnamon", "clove", "coriander"
         , "cumin", "curry", "dill", "fennel", "fenugreek", "ginger", "lavender", "marjoram", "mint", "mustard"
         , "nutmeg", "oregano", "paprika", "parsley", "rosemary", "saffron", "sage", "safflower", "tarragon"
-        , "thyme", "turmeric", "vanilla", "salt", "powder"
+        , "thyme", "turmeric", "vanilla", "salt", "powder", "dried onion", "dried minced onion"
         ] 
     , exclusions = [ "fresh" ]
     }
@@ -51,6 +62,10 @@ defaultCategories = [
     , matchers = [ "egg", "milk", "cream", "cheese", "yogurt", "cheddar", "swiss", "butter", "jack", "colby" ] 
     , exclusions = []
     }  
+  , { name = "Beverages"
+    , matchers = [ "pop", "soda", "water", "juice", "tea", "coke", "sprite", "dr", "dr pepper" ]
+    , exclusions = []
+    }
   , { name = "Snacks"
     , matchers = [ "chips", "chocolate", "crackers" ]
     , exclusions = [] 
