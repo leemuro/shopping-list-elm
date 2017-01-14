1530ac645666a806bf25a1c3b1cb6edd3879f35c
Lee Muro <Lee Muro>
2017-01-14 14:31:40 -0500

    fixes cache busting

--- make.ps1
+++ make.ps1
@@ -1,4 +1,4 @@
 elm-css stylesheets.elm
 elm-make shoppinglist.elm --output app.js
 
-(Get-Content index.html).replace('{cacheToken}', (get-date -Format "MMddyyyyHHmmss")) | Set-Content index.html
+(Get-Content _index.html).replace('{cacheToken}', (get-date -Format "MMddyyyyHHmmss")) | Set-Content index.html
